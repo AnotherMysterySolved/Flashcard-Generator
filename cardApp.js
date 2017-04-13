b92c118f429c310a5969ae2955b4bd10634f7462
AnotherMysterySolved <14mprice@gmail.com>
2017-04-13 10:53:41 -0400

    Functioning now

--- cardApp.js
+++ cardApp.js
@@ -1,14 +1,100 @@
-// Create a BasicCard constructor. It should accept front and back arguments.
+
+var inquirer = require("inquirer"),
+    fs = require('fs');
+
 function BasicCard(front, back){
-  this.front = front;
-  this.back = back;
+    this.front = front;
+    this.back = back;
 }
-// Create a ClozeCard constructor. It should accept text and cloze arguments.
-function ClozeCard(fullText, cloze){}
-// ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
-// ClozeCard should have a property or method that contains or returns only the partial text.
-// ClozeCard should have a property or method that contains or returns only the full text.
-// ClozeCard should throw or log an error when the cloze deletion does not appear in the input text.
-// Use prototypes to attach these methods, wherever possible.
-// As your application will not have a front end, your only need to determine an efficient way to store cloze-deleted cards—you don't have to solve the problem of displaying them. You are free to decide how you'd like to implement this. One might represent the above flashcard thus:
-// The bulk of this assignment is implementing ClozeCard. If you build a robust ClozeCard implementation, feel free to try your hand at implementing a front-end, as well.
+
+
+
+function ClozeCard(text, cloze){
+    this.text = text;
+    this.cloze = cloze;
+
+}
+
+ClozeCard.prototype.partial = function () {
+
+    if(this.text.includes(this.cloze)){
+        return this.text.replace(this.cloze, '....');
+    }
+    else{
+        return "Sorry, value doesn't exist";
+    }
+};
+
+
+
+inquirer.prompt({
+    type: 'list',
+    name: 'cards',
+    message: 'Choose your card?',
+    choices: ['BasicCard', 'Clozure']
+}).then(function (data) {
+    if(data.cards === 'BasicCard'){
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'front',
+                message: 'Add your front message'
+            }, {
+                type: 'input',
+                name: 'back',
+                message: 'Add your back message'
+            }
+        ]);
+    }
+
+    else{
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'Add your text message'
+            }, {
+                type: 'input',
+                name: 'cloze',
+                message: 'Add your deletion message'
+            }
+        ]);
+    }
+
+
+}).then(function(data){
+
+        if(data.front){
+            var basic = new BasicCard(data.front, data.back);
+            addCards({data: basic});
+
+        }
+        else{
+            var firstPresident = new ClozeCard(data.text, data.cloze);
+            addCards({data: firstPresident, partial: firstPresident.partial()});
+        }
+
+
+    })
+    .catch(function (err) {
+     console.log(err);
+});
+
+
+
+
+
+var addCards = function (add) {
+    fs.readFile('./data.json', 'utf8', function (error, data) {
+        if(error) throw error;
+
+        var arr = JSON.parse(data);
+
+        arr.cards.push(add);
+
+        fs.writeFile('./data.json', JSON.stringify(arr), 'utf8', function (err) {
+            if(err) throw err;
+            console.log("Process completed!!!");
+        });
+    });
+};
